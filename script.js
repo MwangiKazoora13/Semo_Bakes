063405f432882b91a2ee32ac262637f375439356
Mwangi Kazoora <mkazoora13@gmail.com>
2025-10-16 10:55:25 +0300

    First commit of Folder files into repository

---
+++ script.js
@@ -0,0 +1,9 @@
+// Sidebar for phone navigation
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
